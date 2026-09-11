023fe50c5b188c3071e1ab0d11d748865e51f0fd
Krish Sisodiya <krishsisodiya7389@gmail.com>
2026-09-11 21:53:12 +0530

    Last Update

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import CategoryPage from "../pages/CategoryPage";
-import Auth from "../pages/Auth";
 
 const AppRoutes = () => {
   return (
@@ -9,11 +8,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/category/:name" element={<CategoryPage />} />
-        
-        {/* Authentication Routes */}
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/register" element={<Auth />} />
+      
       </Routes>
     </BrowserRouter>
   );
